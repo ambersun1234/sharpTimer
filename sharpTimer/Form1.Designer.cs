4f6f0d9f6198cc57f069f678a8990ddc3a305ff5
ambersun1234 <ambersun1019.shawn@gmail.com>
2020-03-26 17:25:05 +0800

    Add basic timer code

---
+++ sharpTimer/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace sharpTimer {
+	partial class Form1 {
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.currentTime = new System.Windows.Forms.Button();
+			this.uitimer = new System.Windows.Forms.Timer(this.components);
+			this.pressBtn = new System.Windows.Forms.Button();
+			this.clearbtn = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// currentTime
+			// 
+			this.currentTime.Enabled = false;
+			this.currentTime.Font = new System.Drawing.Font("新細明體", 30F);
+			this.currentTime.Location = new System.Drawing.Point(182, 49);
+			this.currentTime.Name = "currentTime";
+			this.currentTime.Size = new System.Drawing.Size(429, 67);
+			this.currentTime.TabIndex = 0;
+			this.currentTime.UseVisualStyleBackColor = true;
+			// 
+			// uitimer
+			// 
+			this.uitimer.Interval = 1000;
+			this.uitimer.Tick += new System.EventHandler(this.uitimer_Tick);
+			// 
+			// pressBtn
+			// 
+			this.pressBtn.BackColor = System.Drawing.Color.LawnGreen;
+			this.pressBtn.Font = new System.Drawing.Font("新細明體", 20F);
+			this.pressBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.pressBtn.Location = new System.Drawing.Point(308, 160);
+			this.pressBtn.Name = "pressBtn";
+			this.pressBtn.Size = new System.Drawing.Size(189, 49);
+			this.pressBtn.TabIndex = 1;
+			this.pressBtn.Text = "start";
+			this.pressBtn.UseVisualStyleBackColor = false;
+			this.pressBtn.Click += new System.EventHandler(this.pressBtn_Click);
+			// 
+			// clearbtn
+			// 
+			this.clearbtn.BackColor = System.Drawing.Color.Pink;
+			this.clearbtn.Font = new System.Drawing.Font("新細明體", 20F);
+			this.clearbtn.Location = new System.Drawing.Point(308, 240);
+			this.clearbtn.Name = "clearbtn";
+			this.clearbtn.Size = new System.Drawing.Size(189, 49);
+			this.clearbtn.TabIndex = 2;
+			this.clearbtn.Text = "clear";
+			this.clearbtn.UseVisualStyleBackColor = false;
+			this.clearbtn.Click += new System.EventHandler(this.clearbtn_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.clearbtn);
+			this.Controls.Add(this.pressBtn);
+			this.Controls.Add(this.currentTime);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button currentTime;
+		private System.Windows.Forms.Timer uitimer;
+		private System.Windows.Forms.Button pressBtn;
+		private System.Windows.Forms.Button clearbtn;
+	}
+}
+
---
+++ sharpTimer/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sharpTimer {
+	public partial class Form1: Form {
+		private bool init; // indicate first time
+		private bool check = false;
+		private Button currentTimeBtn;
+		private Button mypressBtn;
+		private Button myclearBtn;
+		private Timer currentTimer;
+		private double systemTimer = 0;
+		private double myinterval = 1;
+
+
+		private enum systemOrder {
+			increase, decrease
+		}; // indicate timer order
+
+		public Form1() {
+			InitializeComponent();
+			this.currentTimeBtn = currentTime;
+			this.currentTimer = uitimer;
+			this.mypressBtn = pressBtn;
+			this.myclearBtn = clearbtn;
+			this.reinit();
+		}
+
+		private String convert() {
+			int tmp = Convert.ToInt32(this.systemTimer);
+
+			int hour = tmp / (60 * 60);
+			tmp -= ((60 * 60) * hour);
+			int minute = tmp / 60;
+			tmp -= (60 * minute);
+			int second = tmp;
+
+			return hour.ToString("00") + ":" + 
+				minute.ToString("00") + ":" + 
+				second.ToString("00");
+		}
+
+		private void tick() {
+			this.systemTimer += 1;
+			// this.systemTimer %= this.myinterval;
+			this.currentTimeBtn.Text = this.convert();
+		}
+
+		private void reinit() {
+			this.currentTimer.Enabled = false;
+			this.currentTimer.Stop();
+			this.systemTimer = 0;
+			this.init = true;
+			this.currentTimeBtn.Enabled = false;
+			this.currentTimeBtn.Text = this.convert();
+			this.currentTimer.Interval = Convert.ToInt32(this.myinterval);
+			this.pressBtn.Text = "start";
+			this.pressBtn.BackColor = Color.LawnGreen;
+			this.myclearBtn.Text = "clear";
+			this.myclearBtn.BackColor = Color.LightPink;
+		}
+
+		private void pressBtn_Click(object sender, EventArgs e) {
+			Color tmp;
+			bool enabler = false;
+
+			if (!this.init) {
+				enabler = false;
+				this.currentTimer.Stop();
+				this.mypressBtn.Text = "start";
+				tmp = Color.LawnGreen;
+			} else {
+				if (this.check) {
+					this.currentTimer.Stop();
+					this.mypressBtn.Text = "continue";
+					tmp = Color.LawnGreen;
+					enabler = false;
+				}
+				else {
+					this.mypressBtn.Text = "pause";
+					enabler = true;
+					tmp = Color.Aqua;
+				}
+			}
+			this.check = !this.check;
+			this.currentTimer.Enabled = enabler;
+			this.mypressBtn.BackColor = tmp;
+		}
+
+		private void uitimer_Tick(object sender, EventArgs e) {
+			this.tick();
+		}
+
+		private void clearbtn_Click(object sender, EventArgs e) {
+			this.reinit();
+		}
+	}
+}
