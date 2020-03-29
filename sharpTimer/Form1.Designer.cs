a18a32b448624d4f6d0d873e492f482d97fc8350
ambersun1234 <ambersun1019.shawn@gmail.com>
2020-03-29 16:42:35 +0800

    Add countdown

--- sharpTimer/Form1.Designer.cs
+++ sharpTimer/Form1.Designer.cs
@@ -31,6 +31,18 @@
             this.stopwatchBtn = new System.Windows.Forms.Button();
             this.countdownBtn = new System.Windows.Forms.Button();
             this.reinitBtn = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.countdown_set = new System.Windows.Forms.Button();
+            this.countdown_clear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // currentTime
@@ -115,11 +127,166 @@
             this.reinitBtn.UseVisualStyleBackColor = false;
             this.reinitBtn.Click += new System.EventHandler(this.initChoose);
             // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.Color.Lime;
+            this.btn_5.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_5.Location = new System.Drawing.Point(165, 106);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(50, 50);
+            this.btn_5.TabIndex = 6;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = false;
+            this.btn_5.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.Color.Lime;
+            this.btn_6.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_6.Location = new System.Drawing.Point(221, 106);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(50, 50);
+            this.btn_6.TabIndex = 7;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = false;
+            this.btn_6.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.Color.Lime;
+            this.btn_7.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_7.Location = new System.Drawing.Point(277, 106);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(50, 50);
+            this.btn_7.TabIndex = 8;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = false;
+            this.btn_7.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.Color.Lime;
+            this.btn_8.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_8.Location = new System.Drawing.Point(336, 106);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(50, 50);
+            this.btn_8.TabIndex = 9;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = false;
+            this.btn_8.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.BackColor = System.Drawing.Color.Lime;
+            this.btn_9.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_9.Location = new System.Drawing.Point(392, 106);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(50, 50);
+            this.btn_9.TabIndex = 10;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = false;
+            this.btn_9.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.Color.Lime;
+            this.btn_4.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_4.Location = new System.Drawing.Point(392, 162);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(50, 50);
+            this.btn_4.TabIndex = 15;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = false;
+            this.btn_4.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.Color.Lime;
+            this.btn_3.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_3.Location = new System.Drawing.Point(336, 162);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(50, 50);
+            this.btn_3.TabIndex = 14;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = false;
+            this.btn_3.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.Color.Lime;
+            this.btn_2.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_2.Location = new System.Drawing.Point(277, 162);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(50, 50);
+            this.btn_2.TabIndex = 13;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = false;
+            this.btn_2.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.Lime;
+            this.btn_1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_1.Location = new System.Drawing.Point(221, 162);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(50, 50);
+            this.btn_1.TabIndex = 12;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            this.btn_1.Click += new System.EventHandler(this.setter_click);
+            // 
+            // btn_0
+            // 
+            this.btn_0.BackColor = System.Drawing.Color.Lime;
+            this.btn_0.Font = new System.Drawing.Font("新細明體", 20F);
+            this.btn_0.Location = new System.Drawing.Point(165, 162);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(50, 50);
+            this.btn_0.TabIndex = 11;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = false;
+            this.btn_0.Click += new System.EventHandler(this.setter_click);
+            // 
+            // countdown_set
+            // 
+            this.countdown_set.BackColor = System.Drawing.Color.Lime;
+            this.countdown_set.Font = new System.Drawing.Font("新細明體", 20F);
+            this.countdown_set.Location = new System.Drawing.Point(165, 225);
+            this.countdown_set.Name = "countdown_set";
+            this.countdown_set.Size = new System.Drawing.Size(136, 40);
+            this.countdown_set.TabIndex = 16;
+            this.countdown_set.Text = "set";
+            this.countdown_set.UseVisualStyleBackColor = false;
+            this.countdown_set.Click += new System.EventHandler(this.countdown_set_Click);
+            // 
+            // countdown_clear
+            // 
+            this.countdown_clear.Font = new System.Drawing.Font("新細明體", 20F);
+            this.countdown_clear.Location = new System.Drawing.Point(306, 225);
+            this.countdown_clear.Name = "countdown_clear";
+            this.countdown_clear.Size = new System.Drawing.Size(136, 40);
+            this.countdown_clear.TabIndex = 17;
+            this.countdown_clear.Text = "clear";
+            this.countdown_clear.UseVisualStyleBackColor = true;
+            this.countdown_clear.Click += new System.EventHandler(this.countdown_clear_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.countdown_clear);
+            this.Controls.Add(this.countdown_set);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
             this.Controls.Add(this.reinitBtn);
             this.Controls.Add(this.countdownBtn);
             this.Controls.Add(this.stopwatchBtn);
@@ -142,6 +309,18 @@
         private System.Windows.Forms.Button stopwatchBtn;
         private System.Windows.Forms.Button countdownBtn;
         private System.Windows.Forms.Button reinitBtn;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button countdown_set;
+        private System.Windows.Forms.Button countdown_clear;
     }
 }
 
--- sharpTimer/Form1.cs
+++ sharpTimer/Form1.cs
@@ -12,37 +12,53 @@ namespace sharpTimer {
 	public partial class Form1: Form {
 		private bool init; // indicate first time
 		private bool check = false;
+        private const int showTextLen = 6;
+        private int set_counter = Form1.showTextLen; // 8 - 1
 		private Button currentTimeBtn;
 		private Button mypressBtn; // start, pause, continue
 		private Button myclearBtn; // clear
-        private Button stopwatch;  // init btn
-        private Button countdown;  // init btn
-        private Button myreinit;     // init btn
+        private Button myreinit;   // init btn
 		private Timer currentTimer;
 		private double systemTimer = 0;
         private double bpSystemTimer = 0;
-		private double myinterval = 1;
+		private const double myinterval = 100;
+        private systemOrder current;
+        private int[] indexArr = new int[] { 0, 1, 3, 4, 6, 7 };
         private List<Button> stopwatchList = new List<Button>();
         private List<Button> countdownList = new List<Button>();
         private List<Button> initList = new List<Button>();
 
-		public Form1() {
+        private enum systemOrder
+        {
+            increase, decrease
+        }; // indicate timer order
+
+        public Form1() {
 			InitializeComponent();
 			this.currentTimeBtn = currentTime;
 			this.currentTimer = uitimer;
 			this.mypressBtn = pressBtn;
 			this.myclearBtn = clearbtn;
-            this.stopwatch = stopwatchBtn;
-            this.countdown = countdownBtn;
             this.myreinit = reinitBtn;
 
-            this.initList.Add(this.stopwatch);
-            this.initList.Add(this.countdown);
+            this.initList.Add(stopwatchBtn);
+            this.initList.Add(countdownBtn);
+
+            this.countdownList.Add(btn_0);
+            this.countdownList.Add(btn_1);
+            this.countdownList.Add(btn_2);
+            this.countdownList.Add(btn_3);
+            this.countdownList.Add(btn_4);
+            this.countdownList.Add(btn_5);
+            this.countdownList.Add(btn_6);
+            this.countdownList.Add(btn_7);
+            this.countdownList.Add(btn_8);
+            this.countdownList.Add(btn_9);
+            this.countdownList.Add(countdown_clear);
+            this.countdownList.Add(countdown_set);
 
             this.stopwatchList.Add(this.mypressBtn);
             this.stopwatchList.Add(this.myclearBtn);
-
-            // this.countdownList.Add(Button);
 
 			this.reinit();
 		}
@@ -64,19 +80,35 @@ namespace sharpTimer {
 			tmp -= (60 * minute);
 			int second = tmp;
 
-			return hour.ToString("00") + ":" + 
+            string stmp = "";
+            if (hour > 99)
+            {
+                stmp = hour.ToString("000");
+            } else
+            {
+                stmp = hour.ToString("00");
+            }
+			return stmp + ":" + 
 				minute.ToString("00") + ":" + 
 				second.ToString("00");
 		}
 
 		private void tick() {
-			this.systemTimer += 1;
-			// this.systemTimer %= this.myinterval;
-			this.currentTimeBtn.Text = this.convert();
+            if (this.current == Form1.systemOrder.decrease && this.systemTimer == 0)
+            {
+                return;
+            }
+
+            this.systemTimer =
+            (this.current == Form1.systemOrder.increase ?
+            this.systemTimer + 1 : this.systemTimer - 1);
+
+            this.currentTimeBtn.Text = this.convert();
 		}
 
 		private void reinit() {
-			this.currentTimer.Enabled = false;
+            this.set_counter = Form1.showTextLen;
+            this.currentTimer.Enabled = false;
 			this.currentTimer.Stop();
 			this.bpSystemTimer= 0;
             this.systemTimer = this.bpSystemTimer;
@@ -84,7 +116,7 @@ namespace sharpTimer {
             this.check = false;
 			this.currentTimeBtn.Enabled = false;
 			this.currentTimeBtn.Text = this.convert();
-			this.currentTimer.Interval = Convert.ToInt32(this.myinterval);
+			this.currentTimer.Interval = Convert.ToInt32(Form1.myinterval);
 			this.pressBtn.Text = "start";
 			this.pressBtn.BackColor = Color.LawnGreen;
 			this.myclearBtn.Text = "clear";
@@ -122,6 +154,23 @@ namespace sharpTimer {
 			this.mypressBtn.BackColor = tmp;
 		}
 
+        private void setter_click(object sender, EventArgs e)
+        {
+            if (this.set_counter > 0)
+            {
+                char input = char.Parse(((Button)sender).Text);
+                char[] tmp = this.currentTimeBtn.Text.ToCharArray();
+
+                for (int i = 1; i < this.indexArr.Length; i++)
+                {
+                    tmp[this.indexArr[i - 1]] = tmp[this.indexArr[i]];
+                }
+                tmp[this.indexArr[this.indexArr.Length - 1]] = input;
+                this.currentTimeBtn.Text = new string(tmp);
+                this.set_counter -= 1;
+            }
+        }
+
 		private void uitimer_Tick(object sender, EventArgs e) {
 			this.tick();
 		}
@@ -142,17 +191,48 @@ namespace sharpTimer {
 
             if (tmp == "stopwatch")
             {
+                this.current = Form1.systemOrder.increase;
                 this.bpSystemTimer = 0;
                 this.setButtonVisible(this.stopwatchList, true);
                 this.setButtonVisible(this.initList, false);
             } else if (tmp == "countdown")
             {
+                this.current = Form1.systemOrder.decrease;
                 this.setButtonVisible(this.countdownList, true);
                 this.setButtonVisible(this.initList, false);
             } else
             {
                 this.reinit();
             }
+        }
+
+        private void countdown_clear_Click(object sender, EventArgs e)
+        {
+            this.set_counter = Form1.showTextLen;
+            this.currentTimeBtn.Text = this.convert();
+        }
+
+        private void countdown_set_Click(object sender, EventArgs e)
+        {
+            char[] tmp = this.currentTimeBtn.Text.ToCharArray();
+
+            int stmp = ((int)Char.GetNumericValue(tmp[7]) + (int)Char.GetNumericValue(tmp[6]) * 10);
+            double second = (double)(stmp % 60);
+
+            int mtmp = ((int)Char.GetNumericValue(tmp[4]) + (int)Char.GetNumericValue(tmp[3]) * 10);
+            int mmtmp = mtmp + (stmp / 60);
+            double minute = (double)(mmtmp % 60);
+
+            int htmp = ((int)Char.GetNumericValue(tmp[1]) + (int)Char.GetNumericValue(tmp[0]) * 10);
+            double hour = (double)(htmp) + (double)(mmtmp / 60);
+
+            this.bpSystemTimer = hour * 60 * 60 + minute * 60 + second;
+            this.systemTimer = this.bpSystemTimer;
+            this.currentTimeBtn.Text = this.convert();
+
+            this.setButtonVisible(this.countdownList, false);
+            this.setButtonVisible(this.stopwatchList, true);
+            this.setButtonVisible(this.initList, false);
         }
     }
 }
